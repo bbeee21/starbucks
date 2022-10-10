fc3b85e5906494fd4152de5bc0e938507c187b78
tom <bbeee21@hanyang.ac.kr>
2022-10-11 00:14:38 +0900

    rewards 완성

--- js/main.js
+++ js/main.js
@@ -57,3 +57,37 @@ new Swiper('.notice-line .swiper', {
   autoplay: true,
   loop: true
 });
+
+
+// new Swiper(선택자, 옵션)
+new Swiper('.promotion .swiper', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어
+  },
+  navigation: {
+    prevEl : '.promotion .swiper-button-prev',
+    nextEl : '.promotion .swiper-button-next'
+  }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if (isHidePromotion) {
+    // 숨김 처리!
+    promotionEl.classList.add('hide');
+  } else {
+    // 보임 처리!
+    promotionEl.classList.remove('hide');
+  }
+})
